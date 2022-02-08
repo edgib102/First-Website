eaa204146f89184effc43b3a51ec44a13c1cf997
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-02-08 22:35:10 +1300

    automatic

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 $(function () {
 
+
+    
+    for (const [index] of Object.entries(data)) {
+        var filePath = data[index].file;
+
+        $(".images").append(`<div class="${index}" style="opacity:100" data-tippy-content = "${index}"></div>`)
+        $(`.${index}`).append(`<img src="${filePath}" alt="">`)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     tippy('[data-tippy-content]', { followCursor: true });
 
     $('.cryptomatte > div').on('click', function() {
@@ -7,14 +29,9 @@ $(function () {
         var elemText = $(elem).attr("data-tippy-content");
         var sidebar = $('.sidetext');
 
-        sidebar.html(elemText);
+        sidebar.html(elemText + 'ypp');
         
     });
-
-    $.getJSON('data.json', function(data){
-        console.log(data.object-data)
-    })
-
 });
 
 
---
+++ data.js
@@ -0,0 +1,21 @@
+const data = {
+    "001": {
+        "file": "images2/Box.png",
+    },
+    "002": {
+        "file": "images2/Floor.png",
+    },
+    "003": {
+        "file": "images2/Person.png",
+    },
+    "004":{
+        "file": "images2/Cube2.png"
+    },
+    "005":{
+        "file": "images2/Top pillar.png"
+    },
+    "006":{
+        "file": "images2/Trees.png"
+    }
+    
+};
