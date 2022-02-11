804305b34c07e6ea9f1a78d54f937468a1cc3396
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-02-11 21:33:39 +1300

    Ver 2 commit

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+const ctx = document.createElement("canvas").getContext("2d");
+var images = [];
+
+
 $(function () {
 
-
-    
     for (const [index] of Object.entries(data)) {
         var filePath = data[index].file;
 
@@ -9,29 +11,62 @@ $(function () {
         $(`.${index}`).append(`<img src="${filePath}" alt="">`)
     }
 
+    $(".images img").on("click", img => getNonTransparentImage(img, img.currentTarget));  
 
-
-
-
-
-
-
-
-
-
-
-
-
-    tippy('[data-tippy-content]', { followCursor: true });
-
-    $('.cryptomatte > div').on('click', function() {
-        var elem = $(this);
-        var elemText = $(elem).attr("data-tippy-content");
-        var sidebar = $('.sidetext');
-
-        sidebar.html(elemText + 'ypp');
-        
-    });
 });
+
+
+    function getNonTransparentImage(elem,target) {
+        if (!target.offsetParent) return;
+
+        const isImage = /img/i.test(target.tagName),
+        x = elem.pageX - target.offsetParent.offsetLeft,
+        y = elem.pageY - target.offsetParent.offsetTop,
+        w = ctx.canvas.width = target.width,
+        h = ctx.canvas.height = target.height;
+        
+        var alpha;
+
+        if(isImage){            
+            ctx.drawImage(target, 0, 0, w, h);
+            alpha = ctx.getImageData(x, y, 1, 1).data[3];            
+        }
+
+        if (alpha === 0){
+            target.hidden = 1
+            images.push(target);
+
+            var clickedElem = document.elementFromPoint(elem.clientX, elem.clientY);
+            console.log(clickedElem)
+            getNonTransparentImage(elem, img.currentTarget);
+
+        } else {
+            images.forEach(el => {
+                el.hidden = 0
+            });
+
+            images = []
+
+            //do stuff
+            
+        }
+
+    }
+
+
+
+
+
+    // tippy('[data-tippy-content]', { followCursor: true });
+
+    // $('.cryptomatte > div').on('click', function() {
+    //     var elem = $(this);
+    //     var elemText = $(elem).attr("data-tippy-content");
+    //     var sidebar = $('.sidetext');
+
+    //     sidebar.html(elemText + 'ypp');
+        
+    // });
+
 
 
