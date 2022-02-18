cc5994bcd4f753f2266134e711cef7096a834e25
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-02-19 11:11:17 +1300

    js functionality

--- data.js
+++ data.js
@@ -1,21 +1,27 @@
 const data = {
     "001": {
         "file": "images2/Box.png",
+        "name": "Box"
     },
     "002": {
         "file": "images2/Floor.png",
+        "name": "Floor"
     },
     "003": {
         "file": "images2/Person.png",
+        "name": "Person"
     },
     "004":{
-        "file": "images2/Cube2.png"
+        "file": "images2/Cube2.png",
+        "name": "Cube 2"
     },
     "005":{
-        "file": "images2/Top pillar.png"
+        "file": "images2/Top pillar.png",
+        "name": "Top Pillar"
     },
     "006":{
-        "file": "images2/Trees.png"
+        "file": "images2/Trees.png",
+        "name": "Tree"
     }
     
 };
--- image.js
+++ image.js
@@ -1,72 +1,79 @@
-const ctx = document.createElement("canvas").getContext("2d");
-var images = [];
+  // Canvas and images
+  const ctx = document.createElement("canvas").getContext("2d");
+  var images = [];
+
+  // Ready functions
+  $(function () {
+      for (const [index] of Object.entries(data)) {
+          var filePath = data[index].file;
+          var name = data[index].name;
 
 
-$(function () {
-
-    for (const [index] of Object.entries(data)) {
-        var filePath = data[index].file;
-
-        $(".images").append(`<div class="${index}" style="opacity:100" data-tippy-content = "${index}"></div>`)
-        $(`.${index}`).append(`<img src="${filePath}" alt="">`)
-    }
-
-    $(".images img").on("click", img => getNonTransparentImage(img, img.currentTarget));  
-
-});
-
-
-    function getNonTransparentImage(elem,target) {
-        if (!target.offsetParent) return;
-
-        const isImage = /img/i.test(target.tagName),
-        x = elem.pageX - target.offsetParent.offsetLeft,
-        y = elem.pageY - target.offsetParent.offsetTop,
-        w = ctx.canvas.width = target.width,
-        h = ctx.canvas.height = target.height;
-        
-        var alpha;
-
-        if(isImage){            
-            ctx.drawImage(target, 0, 0, w, h);
-            alpha = ctx.getImageData(x, y, 1, 1).data[3];            
+          $(".main-images").append(`<img src="${filePath}" class="selector-images" name="${name}" alt="">`)
         }
 
-        if (alpha === 0){
-            target.hidden = 1
-            images.push(target);
+      //tippy('[data-tippy-content]', { followCursor: false, trigger: 'click' });
 
-            var clickedElem = document.elementFromPoint(elem.clientX, elem.clientY);
-            console.log(clickedElem)
-            getNonTransparentImage(elem, img.currentTarget);
+      // Add click to each image
+      $(".main-images img").on("click", img => getNonTransparentImage(img, img.currentTarget));
 
-        } else {
-            images.forEach(el => {
-                el.hidden = 0
-            });
-
-            images = []
-
-            //do stuff
-            
-        }
-
-    }
+  })
 
 
+  function getNonTransparentImage(elem, target) {
+      if (!target.offsetParent) return;
+
+      // Get click coordinates
+      const isImage = /img/i.test(target.tagName),
+          x = elem.pageX - target.offsetParent.offsetLeft,
+          y = elem.pageY - target.offsetParent.offsetTop,
+          w = ctx.canvas.width = target.width,
+          h = ctx.canvas.height = target.height;
+
+      // Draw image to canvas and read Alpha channel value
+      var alpha;
+      if (isImage) {
+          ctx.drawImage(target, 0, 0, w, h);
+          // Returns RGBA values
+          // https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData
+          alpha = ctx.getImageData(x, y, 1, 1).data[3];
+      }
 
 
-
-    // tippy('[data-tippy-content]', { followCursor: true });
-
-    // $('.cryptomatte > div').on('click', function() {
-    //     var elem = $(this);
-    //     var elemText = $(elem).attr("data-tippy-content");
-    //     var sidebar = $('.sidetext');
-
-    //     sidebar.html(elemText + 'ypp');
-        
-    // });
+      /* 
+      * If the clicked pixel's alpha is 0
+      * then hide the image and push it to image stack.
+      */
 
 
+      if (alpha === 0) {
+          //Hide image  
+              
+          target.hidden = 1
+          // Push to images array       
+          images.push(target);
 
+
+          // recursive function  
+          // Get element at clicked position
+          var clickedElem = document.elementFromPoint(elem.clientX, elem.clientY);
+          return getNonTransparentImage(elem, clickedElem);
+      } else {
+
+          // Else, we have clicked on non tranparent part of the image
+          // So find image in the list and set it to visible
+          images.forEach(el => {
+              el.hidden = 0
+          });
+
+          // Reset images
+          images = [];
+          
+          var elemText = $(target).attr("name");
+          console.log(elemText);
+
+          $(".object-title-text").html(elemText)
+
+     
+      }
+  }
