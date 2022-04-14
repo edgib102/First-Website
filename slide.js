37c202019114e30b2d222f39fb6abe2fdba92beb
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-04-15 01:16:47 +1200

    MVP Carousel

--- slide.js
+++ slide.js
@@ -2,7 +2,14 @@ $(function (){
 
     $(document).ready(function(){
         $('.collectors').slick({
-          arrows: true
+        //   arrows: true
+            // autoplay: true,
+            // autoplaySpeed: 3000,
+            // fade: true,
+            speed: 600,
+            // nextArrow: '<style>width:50px</style>'
+            // nextArrow:"<button type='button' class='slick-next pull-right'></button>"
+            prevArrow: ''
         });
       });
                       
