06f116f4fb3f6070763770b5081febd6d96730ae
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-05-14 21:52:46 +1200

    Bugfixes

--- site.js
+++ site.js
@@ -4,12 +4,18 @@ $(function (){
         window.alert("Unfortunately, I haven't started the mint yet. Come back to this once I do!");
     });
 
-    $(document).ready(function(){
-        $('.collectors').slick({
-            speed: 600,
-            prevArrow: ''
-        });
+    // #(document).read
+
+    $('.collectors').slick({
+        speed: 600,
+        prevArrow: false,
+        nextArrow: $('.slideshow-button')
     });
+
+    // $('.slideshow-button').click(function(){
+    //     $(this).prev().slick('slickNext');
+    // });
+
 
     lastHeight = Math.max($(".collector").height());
 
