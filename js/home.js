b197a195c8fce94cf7f726dbb935b4521bd691df
edgib102 <58235933+edgib102@users.noreply.github.com>
2022-07-17 14:59:40 +1200

    updated for mobile devices

--- js/home.js
+++ js/home.js
@@ -1,18 +1,41 @@
 
 $(document).ready(function() {
+    // slides = 3;
+    slides = slidesCheck();
 
-    
-    $('.carousel').slick({
-        speed: 600,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 1500
+    $(window).on('load resize', function() {
+        width = this.innerWidth;
+        slides = slidesCheck();
+        $('.carousel').slick({
+            speed: 600,
+            infinite: true,
+            slidesToShow: slides,
+            slidesToScroll: 1,
+            autoplay: true,
+            autoplaySpeed: 1500
+        });
     });
+    
+    // $('.carousel').slick({
+    //     speed: 600,
+    //     infinite: true,
+    //     slidesToShow: slides,
+    //     slidesToScroll: 1,
+    //     autoplay: true,
+    //     autoplaySpeed: 1500
+    // });
 
     $('.gallery-button').click(function(){
         window.location.href = "gallery.html";
     });
 
 });
+
+function slidesCheck(){
+    width = this.innerWidth;
+    if(width > 240 && width < 1040){
+        return 1;
+    }else{
+        return 3;
+    }
+}
